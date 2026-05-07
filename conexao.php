e5d266b9f279c8cdc86c322af332aaed66e92acc
julio3323 <34660809+julio3323@users.noreply.github.com>
2026-05-07 19:43:35 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,9 +2,13 @@
 	$dsn = "mysql:host=mysql.railway.internal;dbname=railway;charset=utf8";
 	$usuario = "root";
 	$senha = "IQwFILBxneApDfoQuQzeaQEiCrDyHyVF";
+	$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
 	try{
-		$PDO = new PDO($dsn, $usuario, $senha);
+		$PDO = new PDO($dsn, $usuario, $senha, $options);
 
 		//echo "Conectado";
 	}
