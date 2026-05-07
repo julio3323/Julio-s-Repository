c3a5290b17f1136f1a198353c6c28a49315ab07b
julio3323 <34660809+julio3323@users.noreply.github.com>
2026-05-07 20:05:40 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,21 +1,16 @@
 <?php
-$host = "your_railway_host"; // e.g., containers-us-west.railway.app
-$port = "your_railway_port"; 
-$db   = "railway";
-$user = "root";
-$pass = "your_password";
+	$dsn = "mysql:host=mysql.railway.internal;dbname=railway;charset=utf8";
+	$usuario = "root";
+	$senha = "IQwFILBxneApDfoQuQzeaQEiCrDyHyVF";
+	
 
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-];
+	try{
+		$PDO = new PDO($dsn, $usuario, $senha);
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    
-} catch (PDOException $e) {
-    echo json_encode(["error" => $e->getMessage()]);
-}
+		//echo "Conectado";
+	}
+	catch(PDOExcepton $erro){
+		echo "conexao_erro";
+		exit;
+	}
 ?>
-
